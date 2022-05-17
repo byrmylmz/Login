455fab193c42ac176cd7bff57fd8728901930812
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-05-17 08:30:49 +0300

    Initial Commit

---
+++ LoginPage-1/LoginPage_1App.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPage_1App.swift
+//  LoginPage-1
+//
+//  Created by Bayram Yilmaz on 17/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginPage_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
