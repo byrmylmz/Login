8fd77f5ef2efcb33282adec6d111002232606027
bayram <68708205+byrmylmz@users.noreply.github.com>
2022-05-23 20:02:30 +0300

    Simple Login register logic with swift ui

--- LoginPage-1/ContentView.swift
+++ LoginPage-1/ContentView.swift
@@ -9,43 +9,65 @@ import SwiftUI
 
 let lightGreyColor = Color(red: 239.0/255.0, green: 243.0/255.0, blue: 244.0/255.0, opacity: 100)
 
+let storedUsername = "Bayram"
+let storedpassword = "abcabc"
+
 struct ContentView: View {
-    
+
     @State var username: String = ""
     @State var password: String = ""
     
+    @State var authenticationDidFail: Bool = false
+    @State var authenticationDidSucceed: Bool = false
+    
     var body: some View {
-        VStack {
-            HelloText()
-            UserImage()
-            TextField("Username", text: $username)
-                .padding()
-                .background(lightGreyColor)
-                .cornerRadius(5.0)
-                .padding(.bottom,20)
-            SecureField("Password",text: $password)
-                .padding()
-                .background(lightGreyColor)
-                .cornerRadius(5.0)
-                .padding(.bottom,20)
-
-            Button(action: {print("Button Tapped")}){
-                LoginButtonContent()
+        ZStack{
+            VStack {
+                HelloText()
+                UserImage()
+                UserNameTextField(username: $username)
+                PasswordSecureField(password: $password)
+                if authenticationDidFail {
+                    Text("Information is not correct. Try Again.")
+                        .foregroundColor(.red)
+                }
+                
+                Button(action:{
+                    if self.username == storedUsername && self.password == storedpassword
+                    {
+                        self.authenticationDidSucceed = true
+                        self.authenticationDidFail = false
+                    }else
+                    {
+                        self.authenticationDidFail = true
+                        self.authenticationDidSucceed=false
+                    }
+                }){
+                    LoginButtonContent()
+                }
+                
             }
-            
-            
+            .padding()
+            if authenticationDidSucceed{
+                Text("Login Succeded!")
+                    .font(.headline)
+                    .frame(width: 220.0, height: 30.0)
+                    .background(Color.yellow)
+                    .cornerRadius(20.0)
+            }
         }
-        .padding(/*@START_MENU_TOKEN@*/.horizontal/*@END_MENU_TOKEN@*/)
+
     }
 }
 
-
+//view
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
 
+// hello text
 struct HelloText: View {
     var body: some View {
         Text("Hello Bayram!")
@@ -55,16 +77,25 @@ struct HelloText: View {
     }
 }
 
+// User image
 struct UserImage: View {
     var body: some View {
         Image("userImage")
             .resizable()
             .aspectRatio(contentMode: .fill)
             .cornerRadius(150)
-            .frame(width: 150.0, height: 150.0)
+            .frame(width: 170.0, height: 170.0)
+            .clipShape(Circle())
+            .overlay{
+                Circle().stroke(.white, lineWidth: 4)
+            }
+            .shadow(radius: 7)
+            .padding(.bottom, 20.0)
+            
     }
 }
 
+//Login button
 struct LoginButtonContent: View {
     var body: some View {
         Text("LOGIN")
@@ -74,5 +105,31 @@ struct LoginButtonContent: View {
             .frame(width: 220, height: 60)
             .background(Color.black)
             .cornerRadius(35.0)
+    }
+}
+
+struct UserNameTextField: View {
+    
+    @Binding var username: String
+    
+    var body: some View {
+        TextField("Username", text: $username)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom,20)
+    }
+}
+
+struct PasswordSecureField: View {
+    
+    @Binding var password: String
+    
+    var body: some View {
+        SecureField("Password",text: $password)
+            .padding()
+            .background(lightGreyColor)
+            .cornerRadius(5.0)
+            .padding(.bottom,20)
     }
 }
